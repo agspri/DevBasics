e9249d3e3316cdb49d83d11dd5fa4a40eb4be393
agspri <48209437+agspri@users.noreply.github.com>
2021-11-01 14:37:54 +0100

    Added one extension methods for DateTime
    Added two ToFormatYYYYMMdd-Extension Method
    and used a System.Collections.Generic-Extension Method

---
+++ ExtensionMethods/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    static class DateTimeExtensions
+    {
+        public static string ToFormatYYYYMMdd(this DateTime val)
+        {
+            // return $"{val.Year}{val.Month}{val.Day}";
+            return val.ToString("yyyyMMdd");
+        }
+        public static string ToFormatYYYYMMddSep(this DateTime val, string sep)
+        {
+            return val.ToString("yyyy" + sep + "MM" + sep+ "dd");
+        }
+    }
+}
--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;  // <--- extension methods
 
 namespace ExtensionMethods
 {
@@ -6,7 +8,25 @@ namespace ExtensionMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // using Linq extension
+            string[] arr = new string[2] { "elem 1", "elem 2" };
+            IEnumerable<string> list =  arr.Append("appended elem 3");  // Append is a Extension Method
+
+            foreach(var s in list)
+            {
+                Console.WriteLine("arr item: " + s);
+            }
+
+            // use self implemented extension for class DateTime
+            DateTime myDateTime = DateTime.Now;            
+            string dateStr1 = DateTimeExtensions.ToFormatYYYYMMdd(myDateTime);
+            string dateStr2 = myDateTime.ToFormatYYYYMMdd();
+            string dateStr3 = myDateTime.ToFormatYYYYMMddSep(".");
+
+            Console.WriteLine("dateStr1: " + dateStr1);
+            Console.WriteLine("dateStr2: " + dateStr2);
+            Console.WriteLine("dateStr3: " + dateStr3);
+            Console.ReadLine();
         }
     }
 }
