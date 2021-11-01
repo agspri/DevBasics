6432942676e9c5f864701325acf33f3d892af5e1
agspri <48209437+agspri@users.noreply.github.com>
2021-11-01 15:34:15 +0100

    Added DynamicVsVarVsObject Project
    Play with and compare dynamic, var and object

---
+++ DynamicVsVarVsObject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Dynamic;
+
+namespace DynamicVsVarVsObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello";
+            string u = s.ToUpper();   // funzt
+
+            var sv = "Hello";         // Compiler ermittelt automatisch welcher Datentyp gemeint ist.
+            string uv = sv.ToUpper(); // funzt
+
+            object so = "Hello";
+            //string uo = so.ToUpper();  // funzt net!!!  Keine Vorschläge von Intellisense.
+            string uo = ((string)so).ToUpper();   // Mit Casting funzt es.
+
+            dynamic sd = "Hello";      // Compiler ist sozusagen ausgeschaltet.
+                                       // Mit dynamic geht der Compiler davon aus, dass alles koreeckt sein wird
+                                       // zur Laufzeit. Zur Laufzeit muss alles vorhanden sein und passen.
+            string ud = sd.ToUpper();  // Keine Vorschläge von Intellisense.
+                                       //string udXYZ = sd.ToUpperXYZ();  // Keine Fehlermeldung vor Laufzeit. Wird aber zur Laufzeit
+                                       // mit RunTimeBinderException abstuerzen!
+
+            // dynamic objects können um Eigenschaften und Methoden verändert/erweitert werden.
+            dynamic myObj = new ExpandoObject();   // using System.Dynamic;  erforderlich.
+            myObj.Name = "Sepp";
+            Console.WriteLine("myObj.Name = " + myObj.Name);
+
+            myObj.Value = 1;
+            myObj.Increment = (Action)(() => myObj.Value++);
+            Console.WriteLine("myObj.Value = " + myObj.Value);
+            myObj.Increment();
+            Console.WriteLine("myObj.Value = " + myObj.Value);
+
+            Console.WriteLine("Press key");
+        }
+    }
+}
