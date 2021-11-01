acf5af7925094675efae240fbe56130602b11b51
agspri <48209437+agspri@users.noreply.github.com>
2021-11-01 15:01:46 +0100

    Added DesignPatternSingleton Project

---
+++ DesignPatternSingleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DesignPatternSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserContext userContext1 = UserContext.Instance;
+            UserContext userContext2 = UserContext.Instance;
+            Console.WriteLine("Are userContext1 and UserContext.Instance equal? " + (userContext1 == UserContext.Instance));
+            Console.WriteLine("Are userContext1 and userContext2 equal? " + (userContext1 == UserContext.Instance));
+
+            Console.WriteLine("Press any key");
+        }
+    }
+}
---
+++ DesignPatternSingleton/UserContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternSingleton
+{
+    /// <summary>
+    /// Klasse, um Singleton-Pattern auszuprobieren.
+    /// Vorsicht: Ohne Thread-Synchronisation!
+    /// </summary>
+    class UserContext
+    {
+        //// Version 1: early initialization
+        //static UserContext instance = new UserContext();  // 
+        //private UserContext() { }
+
+        //public static UserContext Instance
+        //{
+        //    get
+        //    {
+        //        return instance;
+        //    }
+        //}
+
+        // Version 2: lazy initialization
+        static UserContext instance;
+
+        public int UserId { get; private set; }
+        public int UserName { get; private set; }
+
+        private UserContext() { }
+
+        public void Load()
+        {
+            // load some data
+        }
+        public static UserContext Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new UserContext();
+                return instance;
+            }
+        }
+    }
+}
