3e61f4db6faaa0939fe35bd77e9649d6b230e7b1
agspri <48209437+agspri@users.noreply.github.com>
2021-11-04 22:18:30 +0100

    Added WPF-Calculator Project
    ICommand / DelegateCommad / Binding NumberCommand / Binding OperatorCommand / Binding CurrentValue / INotifyPropertyChanged

---
+++ WPF-Calculator/CalculatorWindowViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Calculator
+{
+    public class CalculatorWindowViewModel : BaseViewModel
+    {
+        public CalculatorWindowViewModel()
+        {
+            this.NumberCommand = new DelegateCommad((value) =>
+              {
+                  int val = int.Parse((string)value);
+                  this.CurrentValue = this.CurrentValue * 10.0 + val;
+              });
+
+            this.OperatorCommand = new DelegateCommad(ExecuteOperation);
+            // this.ExecuteCommand = new DelegateCommad(ExecuteOperation);    Mögliche Verbesserung:
+            // this.OperatorCommand = new DelegateCommad(Operation);            Aufteilung in 2 Commands.
+        }
+
+        void ExecuteOperation(object o)
+        {
+            string op = (string)o;
+
+            if (op == "=")
+            {
+                switch (operatorToExecute)
+                {
+                    case "+":
+                        CurrentValue += lastValue;
+                        break;
+                    case "-":
+                        CurrentValue = lastValue - currValue;
+                        break;
+                    case "*":
+                        CurrentValue *= lastValue;
+                        break;
+                    case "/":
+                        CurrentValue = lastValue / currValue;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                this.operatorToExecute = op;
+                lastValue = currValue;
+                CurrentValue = 0.0D;
+            }
+        }
+
+        double currValue;
+
+        public double CurrentValue
+        {
+            get => currValue;
+            set
+            {
+                if (currValue != value)
+                {
+                    this.currValue = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        double lastValue;
+
+        string operatorToExecute;
+
+        public DelegateCommad NumberCommand { get; set; }
+        public DelegateCommad OperatorCommand { get; set; }
+
+    }
+}
---
+++ WPF-Calculator/DelegateCommad.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_Calculator
+{
+    /// <summary>
+    /// DelegateCommad:
+    /// Nimmt die Benutzeraktionen entgegen und führt einen Code aus.
+    /// Es arbeitet mit Delegates.
+    /// Wird oft auch mit 'RelayCommand' benannt.
+    /// Dieses Command ist in vielen Libraries.
+    /// </summary>
+    public class DelegateCommad : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// prüft, ob das Command ausgeführt werden kann.
+        /// Z.B.: Ist der Button aktiv/klickbar?
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter) => this.canExecute?.Invoke(parameter) ?? true;  // liefert true, wenn kein Filter
+       
+
+        /// <summary>
+        /// führt den dahinterliegenden Code aus. Z.B. der Code, der bei einem Button-Click ausgeführt werden soll.
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter) => this.execute?.Invoke(parameter);
+
+        //
+        // Noch dazu:
+        //
+        readonly Action<object> execute;         // Das object ist ein Command Parameter
+        readonly Predicate<object> canExecute;
+
+        public DelegateCommad(Predicate<object> canExecute, Action<object> execute)
+            => (this.canExecute, this.execute) = (canExecute, execute);
+
+        public DelegateCommad(Action<object> execute) : this(null, execute) { }
+
+        public void RaiseCanExecuteChanged() => this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
