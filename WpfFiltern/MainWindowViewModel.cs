ab4e6a8fb8e9ced4d9ca319ab4f14eb31198aea6
agspri <48209437+agspri@users.noreply.github.com>
2021-11-07 22:05:39 +0100

    Added WpfFiltern Project
    ObservableCollection /  DelegateCommand / ListBox / Filter /
    Binding Filter, UpdateSourceTrigger=PropertyChanged

---
+++ WpfFiltern/MainWindowViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfLibrary;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+
+namespace WpfFiltern
+{
+    public class MainWindowViewModel : NotifyableBaseObject
+    {
+        //System.Collections.ObjectModel.
+        ObservableCollection<Person> _listData = new ObservableCollection<Person>();
+        public ObservableCollection<Person> FilteredList { get; set; } = new ObservableCollection<Person>();
+
+        string _filter = "";
+
+        public MainWindowViewModel()
+        {
+            this._listData.CollectionChanged += (s, e) =>
+            {
+                DoFiltering();
+            };
+
+            this._listData.Add(new Person { FirstName = "Max", LastName = "Muster", Job = "Developer" });
+            this._listData.Add(new Person { FirstName = "Susi", LastName = "Müller", Job = "Sales" });
+            this._listData.Add(new Person { FirstName = "Dev", LastName = "Dave", Job = "Entwickler" });
+
+            this.AddNewPersonCommand = new DelegateCommand((o) =>
+            {
+                this._listData.Add(new Person { FirstName = "Herbert", LastName = "Bossinger", Job = "Boss" });
+            });
+        }
+
+        public string Filter
+        {
+            get { return _filter; }
+            set
+            {
+                if (_filter != value)
+                {
+                    _filter = value;
+                    this.RaisePropertyChanged();
+                    DoFiltering();
+                }
+            }
+        }
+
+        private void DoFiltering()
+        {
+            this.FilteredList.Clear();
+            //string? value = this._filter?.ToLower();
+            string value = this._filter?.ToLower() ?? "";
+
+            foreach (var item in _listData)
+            {
+                if (String.IsNullOrEmpty(value) ||
+                    item.FullName.ToLower().Contains(value) ||
+                     item.Job.ToLower().Contains(value))
+                {
+                    this.FilteredList.Add(item);
+                }
+            }
+            Console.WriteLine("filtering done.");
+        }
+
+        public ICommand AddNewPersonCommand { get; set; }
+    }
+}
+
---
+++ WpfFiltern/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfFiltern
+{
+   public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public string Job { get; set; }
+    }
+}
